7f9a50e0a95f7c884f1b47fe687a1fc43de82cb8
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2024-07-24 14:25:14 +0800

    enhance: added list of IconData

---
+++ lib/features/add_expense/data/sources/add_expense_data_source.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class AddExpenseDataSource {
+  List<IconData> icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.utensils,
+    FontAwesomeIcons.football,
+    FontAwesomeIcons.music,
+    FontAwesomeIcons.laptop,
+    FontAwesomeIcons.cartShopping,
+  ];
+}
